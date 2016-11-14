00fb07b111e6ddf7623b233b523ed763f74ebed7
Jose <josegante@hotmail.com>
2016-11-14 17:20:10 +0100

    Initial Load

---
+++ SPWA-Angular-solution3/js/app.js
@@ -0,0 +1,80 @@
+(function() {
+    'use strict';
+
+    angular.module('NarrowItDownApp', [])
+        .controller('NarrowItDownController', NarrowItDownController)
+        .service('MenuSearchService', MenuSearchService)
+        .constant('ApiBasePath', "http://davids-restaurant.herokuapp.com")
+        .directive('foundItems', FoundItems);
+
+        function FoundItems() {
+          var ddo = {
+            restrict: "E",
+            templateUrl: 'shoppingList.html',
+            scope: {
+              foundItems: '<',
+              onRemove: '&'
+            }
+          };
+
+          return ddo;
+        }
+
+
+    NarrowItDownController.$inject = ['MenuSearchService'];
+    function NarrowItDownController(MenuSearchService) {
+        var narrow = this;
+        narrow.searchTerm = "orange";
+
+        narrow.doSearch = function() {
+            var promise = MenuSearchService.getMatchedMenuItems(narrow.searchTerm);
+
+            promise.then(function(response) {
+                narrow.items = response;
+            }).catch(function(error) {
+                console.log(error);
+            })
+        }
+
+        narrow.removeItem = function (index) {
+          MenuSearchService.removeItem(index);
+        };
+    }
+
+
+    function MenuSearchService($http, ApiBasePath) {
+        var service = this;
+        var foundItems = [];
+        console.clear();
+
+        service.removeItem = function (index) {
+          foundItems.splice(index, 1);
+        }
+
+        service.getMatchedMenuItems = function(searchTerm) {
+
+            return $http({
+                    method: "GET",
+                    url: (ApiBasePath + "/menu_items.json"),
+                }).then(function(result) {
+                   foundItems = [];
+
+                    for (var i = 0; i < result.data.menu_items.length; i++) {
+                        var description = result.data.menu_items[i].description;
+                        if (description.toLowerCase().indexOf(searchTerm) !== -1) {
+                            foundItems.push({
+                                name: result.data.menu_items[i].name,
+                                description: result.data.menu_items[i].description
+                            });
+                        }
+                    }
+                    return foundItems;
+                })
+                .catch(function(error) {
+                    console.log(error);
+                })
+        };
+
+    }
+
+})();
